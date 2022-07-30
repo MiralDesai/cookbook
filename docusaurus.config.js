2a58b31ca99e3a759c33dfea70f651329cdbd2d7
Miral Desai <13769268+MiralDesai@users.noreply.github.com>
2022-07-30 14:14:29 +0100

    feat: configure search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,12 +58,21 @@ const config = {
         },
       },
     ],
-    // [
-    //   require.resolve("@easyops-cn/docusaurus-search-local"), {
-    //     hashed: true,
-    //     language: ["en"],
-    //   },
-    // ],
+  ],
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
   ],
 };
 
